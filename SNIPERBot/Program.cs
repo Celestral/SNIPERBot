67b664e66b17c6741269323a73679c84399bb54f
Alexandra Belzon <a.belzon@gmail.com>
2022-11-19 15:36:39 +0100

    Restructured to Dependency Injection

--- SNIPERBot/Program.cs
+++ SNIPERBot/Program.cs
@@ -1,26 +1,46 @@
 ﻿using Discord;
+using Discord.Interactions;
 using Discord.Net;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using SNIPERBot;
+using System.Reflection;
 
 public class Program
 {
-    DiscordSocketClient client;
-    List<Project> _projects = new List<Project>();
+    private DiscordSocketClient _client;
+    private InteractionService _interactionService;
+    private readonly ulong _guildId = 990594846563647578;
+    private readonly IServiceProvider _serviceProvider;
 
-    static void Main(string[] args) => new Program().RunAsync(args).GetAwaiter().GetResult();
+    public Program()
+    {
+        _serviceProvider = CreateProvider();
+    }
 
-    async Task RunAsync(string[] args)
+    static IServiceProvider CreateProvider()
     {
         var config = new DiscordSocketConfig()
         {
             GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildPresences
         };
 
-        client = new DiscordSocketClient(config);
+        var collection = new ServiceCollection()
+            .AddSingleton(config)
+            .AddSingleton<DiscordSocketClient>();
 
-        client.Log += async (msg) =>
+        return collection.BuildServiceProvider();
+    }
+
+    static void Main(string[] args) => new Program().RunAsync(args).GetAwaiter().GetResult();
+
+    async Task RunAsync(string[] args)
+    {
+        _client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+       
+
+        _client.Log += async (msg) =>
         {
             await Task.CompletedTask;
             Console.WriteLine(msg);
@@ -28,42 +48,28 @@ public class Program
 
         var token = File.ReadAllText("token.txt");
 
-        await client.LoginAsync(TokenType.Bot, token);
-        await client.StartAsync();
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
 
-        client.Ready += Client_Ready;
-        client.SlashCommandExecuted += SlashCommandHandler;
-        client.ModalSubmitted += Client_ModalSubmitted;
-        client.ButtonExecuted += ButtonHandler;
+        _client.Ready += Client_Ready;
 
         await Task.Delay(-1);
     }
 
     public async Task Client_Ready()
     {
-        if (File.Exists("projects.json"))
-        {
-            _projects = JsonConvert.DeserializeObject<List<Project>>(File.ReadAllText("projects.json"));
-        }
-
-        var addProjectCommand = new SlashCommandBuilder()
-        .WithName("add-project")
-        .WithDescription("Add a project to the bot!");
-
-        var getProjectsCommand = new SlashCommandBuilder()
-        .WithName("get-projects")
-        .WithDescription("Gets the list for all registered projects");
-
-        var deleteProjectCommand = new SlashCommandBuilder()
-        .WithName("delete-project")
-        .WithDescription("Delete a project from the tag roles list")
-        .AddOption("project", ApplicationCommandOptionType.String, "The project you want to delete", isRequired: true);
-
         try
         {
-            await client.CreateGlobalApplicationCommandAsync(getProjectsCommand.Build());
-            await client.CreateGlobalApplicationCommandAsync(addProjectCommand.Build());
-            await client.CreateGlobalApplicationCommandAsync(deleteProjectCommand.Build());
+            _interactionService = new InteractionService(_client);
+            await _interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+            await _interactionService.RegisterCommandsToGuildAsync(_guildId);
+
+            _client.InteractionCreated += async interaction =>
+            {
+                var scope = _serviceProvider.CreateScope();
+                var ctx = new SocketInteractionContext(_client, interaction);
+                await _interactionService.ExecuteCommandAsync(ctx, scope.ServiceProvider);
+            };
         }
         catch (ApplicationCommandException e)
         {
@@ -71,169 +77,4 @@ public class Program
             Console.WriteLine(json);
         }
     }
-    public async Task ButtonHandler(SocketMessageComponent component)
-    {
-        switch (component.Data.CustomId)
-        {
-            case "delete-button":
-                await DeleteProject(component);
-                break;
-        }
-    }
-
-    private async Task Client_ModalSubmitted(SocketModal arg)
-    {
-        // Get the values of components.
-        List<SocketMessageComponentData> components =
-            arg.Data.Components.ToList();
-
-        var project = new Project();
-        project.Id = _projects.Any() ? _projects.Max(x => x.Id) + 1 : 1;
-        project.Name = components
-            .First(x => x.CustomId == "project_name").Value;
-        project.Description = components
-            .First(x => x.CustomId == "project_description").Value;
-        project.Twitter = components
-            .First(x => x.CustomId == "project_twitter").Value;
-        project.Discord = components
-            .First(x => x.CustomId == "project_discord").Value;
-
-        _projects.Add(project);
-
-        var json = JsonConvert.SerializeObject(_projects);
-        File.WriteAllText("projects.json", json);
-
-
-        // Build the message to send.
-        //string message = "Project " + project.Name + " has been added by " + arg.User.Mention;
-
-        // Specify the AllowedMentions so we don't actually ping everyone.
-        AllowedMentions mentions = new AllowedMentions();
-        mentions.AllowedTypes = AllowedMentionTypes.Users;
-
-        var buttons = new ComponentBuilder()
-                .WithButton("Updates", "update-button", ButtonStyle.Primary, new Emoji("✨"))
-                .WithButton("Whitelist", "whitelist-button")
-                .WithButton("Delete", "delete-button", ButtonStyle.Danger);
-
-        var twitterField = new EmbedFieldBuilder()
-        .WithName("Twitter")
-        .WithIsInline(true);
-
-        if (!string.IsNullOrEmpty(project.Twitter))
-        {
-            twitterField.WithValue(project.Twitter);
-        }
-
-        var discordField = new EmbedFieldBuilder()
-        .WithName("Discord")
-        .WithIsInline(true);
-
-        if (!string.IsNullOrEmpty(project.Discord))
-        {
-            discordField.WithValue(project.Discord);
-        }
-
-        var footer = new EmbedFooterBuilder()
-            .WithText(project.Id.ToString());
-
-        var embedBuiler = new EmbedBuilder()
-                    .WithTitle(project.Name)
-                    .WithDescription(project.Description)
-                    //.AddField(twitterField)
-                    //.AddField(discordField)
-                    .WithColor(Color.Blue)
-                    .WithFooter(footer)
-                    .WithCurrentTimestamp()
-                    .WithAuthor(arg.User);
-
-
-        var guild = client.GetGuild(990594846563647578);
-        var channel = guild.GetChannel(1042928482155499570);
-
-        ISocketMessageChannel socketMessageChannel = (ISocketMessageChannel)channel;
-        await socketMessageChannel.SendMessageAsync(embed: embedBuiler.Build(), components: buttons.Build());
-        await arg.DeferAsync();
-        // Respond to the modal.
-        //await arg.RespondAsync(message, allowedMentions: mentions);
-    }
-
-    private async Task SlashCommandHandler(SocketSlashCommand command)
-    {
-        switch (command.Data.Name)
-        {
-            case "get-projects":
-                await GetProjects(command);
-                break;
-            case "add-project":
-                await AddProject(command);
-                break;
-            case "delete-project":
-                await DeleteProject(command);
-                break;
-        }
-    }
-
-
-    #region Command Implementations
-
-    private async Task GetProjects(SocketSlashCommand command)
-    {
-        var message = "";
-        if (_projects.Count != 0)
-        {
-            foreach (var project in _projects)
-            {
-                message += project.Name + ", ";
-            }
-
-            await command.RespondAsync(message);
-        }
-        else
-        {
-            await command.RespondAsync("No projects have been added yet");
-        }
-    }
-
-    private async Task AddProject(SocketSlashCommand command)
-    {
-
-        var modal = new ModalBuilder()
-       .WithTitle("Project Details")
-       .WithCustomId("project_details")
-       .AddTextInput("Name", "project_name", placeholder: "Project name")
-       .AddTextInput("Description", "project_description", TextInputStyle.Paragraph,
-           "Optional description of project", required: false)
-       .AddTextInput("Twitter", "project_twitter", required: false)
-       .AddTextInput("Discord", "project_discord", required: false);
-
-        await command.RespondWithModalAsync(modal.Build());
-
-    }
-
-    private async Task DeleteProject(SocketMessageComponent component)
-    {
-        var embed = component.Message.Embeds.FirstOrDefault();
-        int projectID = int.Parse(embed.Footer.Value.Text);
-
-        var project = _projects.FirstOrDefault(x => x.Id == projectID);
-
-        _projects.Remove(project);
-
-        var json = JsonConvert.SerializeObject(_projects);
-        File.WriteAllText("projects.json", json);
-
-        await component.Message.DeleteAsync();
-    }
-
-    private async Task DeleteProject(SocketSlashCommand command)
-    {
-        throw new NotImplementedException();
-    }
-
-    #endregion
-
-    #region Helper methods
-
-    #endregion
 }
---
+++ SNIPERBot/ProjectCommandModule.cs
@@ -0,0 +1,156 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNIPERBot
+{
+    public class ProjectCommandModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        private static List<Project> _projects = new List<Project>();
+        private static DiscordSocketClient _client;
+
+        public ProjectCommandModule(DiscordSocketClient client)
+        {
+            if (File.Exists("projects.json"))
+            {
+                _projects = JsonConvert.DeserializeObject<List<Project>>(File.ReadAllText("projects.json"));
+            }
+            _client = client;
+        }
+
+        [SlashCommand("get-projects", "Gets the list for all registered projects")]
+        public async Task GetProjects()
+        {
+            var message = "";
+            if (_projects.Count != 0)
+            {
+                foreach (var project in _projects)
+                {
+                    message += project.Name + ", ";
+                }
+
+                await Context.Interaction.RespondAsync("Gotten through slash command: " + message);
+            }
+            else
+            {
+                await Context.Interaction.RespondAsync("Gotten through slash command: No projects have been added yet");
+            }
+        }
+
+        [SlashCommand("add-project", "Add a project to the bot!")]
+        public async Task AddProject()
+        {
+            await Context.Interaction.RespondWithModalAsync<ProjectModal>("project_modal");
+        }
+
+        [ModalInteraction("project_modal")]
+        public async Task ModalResponse(ProjectModal modal)
+        {
+            var project = new Project();
+            project.Id = _projects.Any() ? _projects.Max(x => x.Id) + 1 : 1;
+            project.Name = modal.Name;
+            project.Description = modal.Description;
+            project.Twitter = modal.Twitter;
+            project.Discord = modal.Discord;
+
+            _projects.Add(project);
+
+            var json = JsonConvert.SerializeObject(_projects);
+            File.WriteAllText("projects.json", json);
+
+            var buttons = new ComponentBuilder()
+                    .WithButton("Updates", "update-button", ButtonStyle.Primary, new Emoji("✨"))
+                    .WithButton("Whitelist", "whitelist-button")
+                    .WithButton("Delete", "delete-button", ButtonStyle.Danger);
+
+            var footer = new EmbedFooterBuilder()
+                .WithText(project.Id.ToString());
+
+            var embedBuiler = new EmbedBuilder()
+                        .WithTitle(project.Name)
+                        .WithDescription(project.Description)
+                        .WithColor(Color.Blue)
+                        .WithFooter(footer)
+                        .WithCurrentTimestamp()
+                        .WithAuthor(Context.User);
+
+            var twitterField = new EmbedFieldBuilder()
+            .WithName("Twitter")
+            .WithIsInline(true);
+
+            if (!string.IsNullOrEmpty(project.Twitter))
+            {
+                twitterField.WithValue(project.Twitter);
+                embedBuiler.AddField(twitterField);
+            }
+
+            var discordField = new EmbedFieldBuilder()
+            .WithName("Discord")
+            .WithIsInline(true);
+
+            if (!string.IsNullOrEmpty(project.Discord))
+            {
+                discordField.WithValue(project.Discord);
+                embedBuiler.AddField(discordField);
+            }
+
+
+            var guild = _client.GetGuild(990594846563647578);
+            var channel = guild.GetChannel(1042928482155499570);
+
+            ISocketMessageChannel socketMessageChannel = (ISocketMessageChannel)channel;
+            await socketMessageChannel.SendMessageAsync(embed: embedBuiler.Build(), components: buttons.Build());
+            await DeferAsync();
+        }
+
+        [ComponentInteraction("delete-button")]
+        private async Task DeleteButton()
+        {
+            await DeleteProject((SocketMessageComponent)Context.Interaction);
+        }
+
+        private async Task DeleteProject(SocketMessageComponent component)
+        {
+            var embed = component.Message.Embeds.FirstOrDefault();
+            int projectID = int.Parse(embed.Footer.Value.Text);
+
+            var project = _projects.FirstOrDefault(x => x.Id == projectID);
+
+            _projects.Remove(project);
+
+            var json = JsonConvert.SerializeObject(_projects);
+            File.WriteAllText("projects.json", json);
+
+            await component.Message.DeleteAsync();
+        }
+    }
+
+    public class ProjectModal : IModal
+    {
+        public string Title => "Add a new project";
+        [InputLabel("Project Name")]
+        [ModalTextInput("project_name", placeholder: "Project name", maxLength: 20)]
+        public string Name { get; set; }
+
+        [RequiredInput(false)]
+        [InputLabel("Description")]
+        [ModalTextInput("project_description", placeholder: "Optional description of project", maxLength: 500)]
+        public string Description { get; set; }
+
+        [RequiredInput(false)]
+        [InputLabel("Twitter")]
+        [ModalTextInput("project_twitter", placeholder: "Twitter link", maxLength: 40)]
+        public string Twitter { get; set; }
+
+        [RequiredInput(false)]
+        [InputLabel("Discord")]
+        [ModalTextInput("project_discord", placeholder: "Discord link", maxLength: 40)]
+        public string Discord { get; set; }
+    }
+}
